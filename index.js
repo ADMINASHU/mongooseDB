8d551aee0d77009dd42936dd90a45ad0ed5dfd5c
ADMINASHU <kashutosh02@gmail.com>
2022-11-24 23:50:17 +0530

    update

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const dbConnect = (DB_URL) => {
   try {
     mongoose.connect(DB_URL, {
       useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
     });
     console.log("database connection successfully....");
   } catch (error) {
